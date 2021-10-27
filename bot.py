a361f5d96232ecd03d3ff46c228a4e852efe6c78
evancole99 <evanrogercole@outlook.com>
2021-10-27 13:52:07 -0400

    resolved merge conflicts

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 # Crypto trade bot
 # Specify API keys in file config.py
+# Strategies can be customized in strategies.py
 # Ensure your API keys are kept secret!
 
 import websocket, json, pprint, talib, numpy
@@ -8,14 +9,9 @@ import config, strategies
 from binance.client import Client
 from binance.enums import *
 
-<<<<<<< HEAD
-
-SOCKET = "wss://stream.binance.com:9443/ws/{}@kline_{}".format(strategies.TRADE_SYMBOL,strategies.KLINE_INTERVAL)
-=======
 symbol_lower = strategies.TRADE_SYMBOL.lower()
 
 SOCKET = "wss://stream.binance.com:9443/ws/{}@kline_{}".format(symbol_lower,strategies.KLINE_INTERVAL)
->>>>>>> 1268adf91165d2538096e0c5d3e14411fb1479c2
 
 closes = []
 
@@ -36,25 +32,18 @@ else:
         print("Error: Invalid strategy selected.")
         exit(1)
     if s == "RSI":
-<<<<<<< HEAD
         strategy = strategies.RSI_strategy()
-    elif s == "BBANDS":
-=======
-        print("RSI strategy selected")
-        strategy = strategies.RSI_strategy()
+
     elif s == "BBANDS":
         print("Bollinger Bands strategy selected")
->>>>>>> 1268adf91165d2538096e0c5d3e14411fb1479c2
         strategy = strategies.BB_strategy()
+
     else:
         print("Error")
         exit(1)
 
-<<<<<<< HEAD
-=======
 strategy_interval = strategy.get_interval()
 print("Interval: {}".format(strategy_interval))
->>>>>>> 1268adf91165d2538096e0c5d3e14411fb1479c2
 
 def order(symbol, side, quantity, order_type=Client.ORDER_TYPE_MARKET):
     try:
@@ -76,11 +65,7 @@ def on_close(ws):
 
 def on_message(ws, message):
     global in_position # ensure function knows variable should be referenced globally
-<<<<<<< HEAD
-    print("message received")
-=======
     # print("message received")
->>>>>>> 1268adf91165d2538096e0c5d3e14411fb1479c2
     json_msg = json.loads(message)
     # pprint.pprint(json_msg)
 
@@ -89,23 +74,13 @@ def on_message(ws, message):
     close = candle['c']
 
     if is_closed:
-<<<<<<< HEAD
         # print("Candle closed at {}".format(close))
-=======
-        print("Candle closed at {}".format(close))
->>>>>>> 1268adf91165d2538096e0c5d3e14411fb1479c2
         closes.append(float(close))
         # print("Num of closes so far: {}".format(len(closes)))
 
         if len(closes) > strategy_interval:
-
             np_closes = numpy.array(closes)
-
-<<<<<<< HEAD
-            signal = strategies.generate_signal(strategy)
-=======
             signal = strategy.signal(np_closes)
->>>>>>> 1268adf91165d2538096e0c5d3e14411fb1479c2
 
             if signal == "SELL":
                 print("SELL SIGNAL RECEIVED")
@@ -113,13 +88,9 @@ def on_message(ws, message):
                 if in_position:
                     print("PLACING SELL ORDER")
                     # binance sell order
-<<<<<<< HEAD
-                    order_success = order(TRADE_SYMBOL, Client.SIDE_SELL, TRADE_QUANTITY, Client.ORDER_TYPE_MARKET)
-=======
                     # subtract binance's 0.1% spot trading fee
                     commission = strategies.TRADE_QUANTITY * 0.001
                     order_success = order(strategies.TRADE_SYMBOL, Client.SIDE_SELL, strategies.TRADE_QUANTITY - commission, Client.ORDER_TYPE_MARKET)
->>>>>>> 1268adf91165d2538096e0c5d3e14411fb1479c2
                     
                     if order_success:
                         in_position = False
@@ -135,15 +106,12 @@ def on_message(ws, message):
                 else:
                     print("PLACING BUY ORDER")
                     # binance buy order
-<<<<<<< HEAD
-                    order_success = order(TRADE_SYMBOL, Client.SIDE_BUY, TRADE_QUANTITY, Client.ORDER_TYPE_MARKET)
-=======
                     order_success = order(strategies.TRADE_SYMBOL, Client.SIDE_BUY, strategies.TRADE_QUANTITY, Client.ORDER_TYPE_MARKET)
->>>>>>> 1268adf91165d2538096e0c5d3e14411fb1479c2
                     
                     if order_success:
                         in_position = True
 
+websocket.enableTrace(True)
 
 ws = websocket.WebSocketApp(SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
 
--- strategies.py
+++ strategies.py
@@ -7,15 +7,9 @@ import talib, numpy
 STRATEGY_LIST = ["RSI", "BBANDS"] # list of all currently implemented strategies
 
 # Bot configuration
-<<<<<<< HEAD
-TRADE_SYMBOL = 'ETHUSD'
-TRADE_QUANTITY = 0.005
-KLINE_INTERVAL = '1m'
-=======
 TRADE_SYMBOL = 'ETHUSDT'
 TRADE_QUANTITY = 0.01
 KLINE_INTERVAL = '3m'
->>>>>>> 1268adf91165d2538096e0c5d3e14411fb1479c2
 
 
 # RSI config
@@ -26,12 +20,9 @@ class RSI_strategy:
         self.RSI_OVERBOUGHT = RSI_OVERBOUGHT
         self.RSI_OVERSOLD = RSI_OVERSOLD
 
-<<<<<<< HEAD
-=======
     def get_interval(self):
         return self.RSI_PERIOD
 
->>>>>>> 1268adf91165d2538096e0c5d3e14411fb1479c2
     def signal(self,closes):
         rsi = talib.RSI(closes, self.RSI_PERIOD)
         rsi = rsi[14:]
@@ -54,7 +45,6 @@ class BB_strategy:
     def __init__(self, BBANDS_PERIOD=20):
         self.BBANDS_PERIOD = BBANDS_PERIOD
 
-<<<<<<< HEAD
     def signal(self,closes):
         upper, middle, lower = talib.BBANDS(closes, timeperiod=self.BBANDS_PERIOD, matype=0)
         upper = upper[-1]
@@ -67,42 +57,8 @@ class BB_strategy:
         if currPrice < lower:
             return "BUY"
         elif currPrice > upper:
-=======
-    def get_interval(self):
-       return self.BBANDS_PERIOD
-
-    def signal(self,closes):
-        upper, middle, lower = talib.BBANDS(closes, timeperiod=self.BBANDS_PERIOD, matype=0)
-        u = upper[-1]
-        m = middle[-1]
-        l = lower[-1]
-        arrLen = len(closes)
-        currPrice = closes[arrLen-1]
-        print("Current BBands: UPPER {}, MIDDLE {}, LOWER {}".format(u, m, l))
-        print("Current close: {}".format(currPrice))
-        if currPrice < l:
-            return "BUY"
-        elif currPrice > u:
->>>>>>> 1268adf91165d2538096e0c5d3e14411fb1479c2
             return "SELL"
         else:
             return None
 
-<<<<<<< HEAD
 
-def get_interval(strategy):
-    if strategy == "RSI":
-        return RSI_strategy.RSI_PERIOD
-    elif strategy == "BBANDS":
-        return BB_strategy.BBANDS_PERIOD
-
-def generate_signal(strategy, closes):
-    if strategy == "RSI":
-        return RSI_strategy.signal(closes)
-    elif strategy == "BBANDS":
-        return BB_strategy.signal(closes)
-    else:
-        return None
-
-=======
->>>>>>> 1268adf91165d2538096e0c5d3e14411fb1479c2
